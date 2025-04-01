3edf0156181d875bdb026f930e76c2aee844b691
khuongtoan <kxtdumbo@gmail.com>
2025-04-01 16:07:32 +0700

    buoi 20

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+
+require("dotenv").config();
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE);
+
+const Tour = mongoose.model(
+	"Tour",
+	{
+		name: String,
+		vehicle: String,
+	},
+	"tours",
+);
+
 const app = express();
 const port = 3000;
+// thiết lập views
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.get('/', (req,res) =>{
-  res.send('Trang chu');
-})
-app.get('/tours', (req,res) =>{
-  res.send('danh sach tour');
-})
+// thiết lập thư mục chứ file tĩnh của fontend
+app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/", (req, res) => {
+	res.render("client/pages/home.pug");
+});
 
+app.get("/tours", async (req, res) => {
+	const tourList = await Tour.find({});
+	console.log(tourList);
+	res.render("client/pages/tour-list.pug", {
+		tourLists: tourList,
+	});
+});
 
-app.listen(port,() => {
-  console.log(`website is running on port ${port}`);
-})
+app.listen(port, () => {
+	console.log(`website is running on port ${port}`);
+});
+
+// mongodb+srv://kxtdumbo:D5hpw3ulBSrLc4jl@project-01-backend-node.7ijbxsg.mongodb.net/tourManagement-project1
