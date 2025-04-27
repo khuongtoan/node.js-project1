c691df412b0202c9bd5f27e9d97470fc68d53809
khuongtoan <kxtdumbo@gmail.com>
2025-04-28 01:03:42 +0700

    validate phan them danh muc

--- routes/admin/category-route.js
+++ routes/admin/category-route.js
@@ -7,11 +7,15 @@ const cloudinaryHelper = require("../../helpers/cloudinary-helpers");
 
 const upload = multer({storage: cloudinaryHelper.storage});
 
-
+const categoryValidate = require("../../validates/admin/category-validate");
 
 router.get("/list", categoryController.list);
 
 router.get("/create", categoryController.create);
-router.post("/create",upload.single('avatar'), categoryController.createPost);
+router.post(
+  "/create",
+  upload.single('avatar'),categoryValidate.createPost,
+  categoryController.createPost
+);
 
 module.exports = router;
