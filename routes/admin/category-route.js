fa9ab2f3a7198744e03de645de70bdc99bd6f605
khuongtoan <kxtdumbo@gmail.com>
2025-05-15 20:31:09 +0700

    xoa danh muc

--- routes/admin/category-route.js
+++ routes/admin/category-route.js
@@ -19,4 +19,6 @@ router.get("/edit/:id", categoryController.edit);
 
 router.patch("/edit/:id", upload.single("avatar"), categoryValidate.createPost, categoryController.editPatch);
 
+router.patch("/delete/:id", categoryController.deletePatch);
+
 module.exports = router;
